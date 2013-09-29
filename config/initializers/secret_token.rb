d51bd0592b4b0265586e8057d32d57ecdacfa51d
Charles Hollenbeck <charles@hollenbeck.pw>
2013-09-29 08:20:18 -0400

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XD6Rails::Application.config.secret_key_base = 'c5b4de7f85879d4ff756c355c8c15d37d73ad656173f9a472f633fc4037c92cd7f77335e6c41ee98b5b98536717aaf33377595e59e88429b074aa1312c11c69a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XD6Rails::Application.config.session_store :cookie_store, key: '_XD6-Rails_session'
